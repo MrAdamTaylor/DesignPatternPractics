952ad99d2fd882de6865877f7e2219171e7cdb36
MrAdamTaylor <admataylorgb@gmail.com>
2023-12-28 06:06:48 +0300

    -Aded Flyweight
    -Aded Flyweight

---
+++ Assets/Scripts/Flyweight/CharacterB.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+class CharacterB : ICharacter
+{
+    private char _actualChatacter = 'b';
+    private string _fontFamily = string.Empty;
+    private int _fontSize;
+    
+    public void Draw(string fontFamily, int fontSize)
+    {
+        _fontFamily = fontFamily;
+        _fontSize = fontSize;
+        Debug.Log($"Отрисовка {_actualChatacter}, {_fontFamily} {_fontSize}");
+    }
+}
